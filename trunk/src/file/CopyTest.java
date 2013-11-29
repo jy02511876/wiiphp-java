66282d7a85b658e22fd4a81b4253aad8ea8ffba3
mf02511876@gmail.com <mf02511876@gmail.com@cba21ee2-808a-1195-8c5a-f71509e8a1f1>
2013-11-29 03:09:42 +0000

    add files folder

--- trunk/src/file/CopyTest.java
+++ trunk/src/file/CopyTest.java
@@ -4,8 +4,8 @@ import java.io.IOException;
 
 public class CopyTest {
 	public static void main(String[] args){
-		String fromName = "copy_test.txt";
-		String toName = "copy_test.txt.bak";
+		String fromName = "files/copy_test.txt";
+		String toName = "files/copy_test.txt.bak";
 		try {
 			FileIO.copyFile(fromName,toName);
 		} catch (IOException e) {
@@ -13,13 +13,13 @@ public class CopyTest {
 			e.printStackTrace();
 		}
 		
-		try {
-			String content = FileIO.renderToString(fromName);
-			System.out.println(content);
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+//		try {
+//			String content = FileIO.renderToString(fromName);
+//			System.out.println(content);
+//		} catch (IOException e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//		}
 		System.out.println("finish");
 	}
 }
