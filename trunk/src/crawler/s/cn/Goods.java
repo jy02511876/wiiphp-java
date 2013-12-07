aecf979f7daaeee953228283c232b7f2b69ace4a
mf02511876@gmail.com <mf02511876@gmail.com@cba21ee2-808a-1195-8c5a-f71509e8a1f1>
2013-12-07 05:49:46 +0000

    use mongodb

--- trunk/src/crawler/s/cn/Goods.java
+++ trunk/src/crawler/s/cn/Goods.java
@@ -3,6 +3,7 @@ package crawler.s.cn;
 import java.io.UnsupportedEncodingException;
 import java.util.ArrayList;
 import java.util.List;
+import org.apache.commons.lang.StringUtils;
 import org.apache.hadoop.hive.serde2.columnar.BytesRefArrayWritable;
 import org.apache.hadoop.hive.serde2.columnar.BytesRefWritable;
 
@@ -16,13 +17,15 @@ public class Goods {
 	private String imageUrl;
 	private List<Object> columns = new ArrayList<Object>();
 	
+	public Goods(){	
+	}
 	
 	public Goods(String title,double price,double delPrice,String url,String imageUrl){
-		this.title = title.trim();
-		this.price = price;
-		this.delPrice = delPrice;
-		this.url = url;
-		this.imageUrl = imageUrl;
+		setTitle(title);
+		setPrice(price);
+		setDelPrice(delPrice);
+		setUrl(url);
+		setImageUrl(imageUrl);
 	}
 	
 	private void initColumns()
@@ -65,6 +68,47 @@ public class Goods {
 	}
 	
 	
+	public String getTitle(){
+		return title.trim();
+	}
+	
+	public void setTitle(String title){
+		this.title = StringUtils.trim(title);
+	}
+	
+	public double getPrice(){
+		return price;
+	}
+	
+	public void setPrice(double price){
+		this.price = price;
+	}
+	
+	public double getDelPrice(){
+		return delPrice;
+	}
+	
+	public void setDelPrice(double delPrice){
+		this.delPrice = delPrice;
+	}
+	
+	public String getUrl(){
+		return url;
+	}
+	
+	public void setUrl(String url){
+		this.url = StringUtils.trim(url);
+	}
+	
+	public String getImageUrl(){
+		return imageUrl;
+	}
+	
+	public void setImageUrl(String imageUrl){
+		this.imageUrl = StringUtils.trim(imageUrl);
+	}
+	
+	
 	public static void main(String[] args){
 		String title = "耐克nike男鞋板鞋运动鞋555272-616";
 		double price = 349.00;
@@ -73,6 +117,7 @@ public class Goods {
 		String imageUrl = "http://images.s.cn/images/goods/20130504/c24e7bcd9f1a37b4.jpg";
 		
 		Goods goods = new Goods(title,price,delPrice,url,imageUrl);
-		goods.getItemValue();
+		//oods.getItemValue();
+		System.out.println(goods.toString());
 	}
 }
---
+++ trunk/src/crawler/util/MongoDB.java
@@ -0,0 +1,58 @@
+package crawler.util;
+
+import java.net.UnknownHostException;
+
+import com.mongodb.BasicDBObject;
+import com.mongodb.DB;
+import com.mongodb.DBCollection;
+import com.mongodb.DBObject;
+import com.mongodb.Mongo;
+import com.mongodb.MongoClient;
+import com.mongodb.MongoClientURI;
+
+public class MongoDB {
+	private static String user = "mongo";
+	private static String password = "123456";
+	private static String host = "master";
+	private static String port = "27017";
+	
+	private static MongoClient mongo = null;
+	private static DB db = null;
+	
+	public static void initMongo(){
+		if(mongo == null){
+			String uri ="mongodb://"+user+":"+password+"@"+host+":"+port;
+			MongoClientURI URI = new MongoClientURI(uri);
+			try {
+				mongo = new MongoClient(URI);
+			} catch (UnknownHostException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+	}
+	
+	
+	public static DB getDB(String DbName){
+		if(mongo == null)
+			initMongo();
+		return mongo.getDB(DbName);
+	}
+	
+	
+	public static void close(){
+		mongo.close();
+	}
+	
+	public static void main(String[] args){
+		
+		DB db = MongoDB.getDB("test");
+		DBCollection collection = db.getCollection("s_cn_list");
+		BasicDBObject obj = new BasicDBObject("title","title")
+								.append("url", "http")
+								.append("image_url", "http://image");
+		collection.insert(obj);
+		MongoDB.close();
+		System.out.println("finish");
+	}
+}
